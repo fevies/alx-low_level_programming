2a2b7d74cd49dc05abba05b3731eb70e5ad08aab
fevies <elviesodhiambo@gmail.com>
2023-03-16 13:57:05 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
  * Programming is like building a multilingual puzzle
  */
 int	main(void)
-{	puts ("\"Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
