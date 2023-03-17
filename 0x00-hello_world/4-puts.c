4ee91beabe27050dd00ca588f4431bfd494fa574
fevies <elviesodhiambo@gmail.com>
2023-03-17 13:43:31 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
-/*
- * Programming is like building a multilingual puzzle
+ #include <stdio.h>
+ /**
+ * main - entry point
+ *return - programs exit point
  */
-int	main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@
 	 */
 	int main(void)
 	{
-		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n"6-size.c);
 		return 0;
 	}
