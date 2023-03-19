b64b58248a51d497ca30d64075dd7128dfd72107
fevies <elviesodhiambo@gmail.com>
2023-03-19 02:36:52 -0700

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,16 +11,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
         if	(n > 5)
 	{
-		printf("%d and is greater than 5");
+		printf("%d and is greater than 5",n);
 	}
 	else if	(n == 0)
 	{
-		printf("%d and is 0");
+		printf("%d and is 0",n);
 
 	}
 	else	(n < 6 && n != 0)
 	{
-		printf("%d and is less than 6 and 0")
+		printf("%d and is less than 6 and 0",n);
 	}
 	return (0);
 }
