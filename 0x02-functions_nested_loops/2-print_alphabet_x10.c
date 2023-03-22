944638141bcd2c0b010f33bcc637082f37c3e76b
fevies <elviesodhiambo@gmail.com>
2023-03-22 12:39:27 -0700

    code to print lowercase alphabet in a sequential order

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,16 @@
 #include "main.h"
+
 /**
- * _islower - A program that would Show 1 if the inputs
- * is a lowercase character. Another case, show 0
  *
- * @c: The character in ASCII code
  *
- * Return: 1 for lowercase. 0 for the rest.
  */
-int _islower(int c)
+
+void lowercase_alphabet(void)
 {
-	if (c >= 'a' && c <= 'z')
+	char lowercasealpha;
+
+	for (lowercasealpha = 'a'; lowercasealpha <= 'z'; lowercasealpha++;)
 	{
-		return (1);
+		_putchar(lowercasealpha);
 	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
 }
