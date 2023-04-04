bab96317b5995bdd9e2aa4d7263f2fb7dc41ee2e
fevies <elviesodhiambo@gmail.com>
2023-04-04 13:52:28 -0700

    a

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int test;
+	while (test=0; test<n; test++)
+	{
+		*(s + i) = b;
+	}
+	return(s);
+}
