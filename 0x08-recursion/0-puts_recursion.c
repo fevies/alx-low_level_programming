bc51d993fede382977d0952d98f489527acb50a7
fevies <elviesodhiambo@gmail.com>
2023-04-05 13:21:48 -0700

    function that prints a string and a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -variable to print a string
+ * return :no value
+ * @s: string
+ */
+void _puts_recursion(char *s);
+{
+	if  (*s != NULL)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	putchar("\n");
+}
