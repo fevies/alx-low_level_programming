0c0a01e2f6502b6da91499e4c6db28788b0cd384
fevies <elviesodhiambo@gmail.com>
2023-04-12 14:35:58 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,16 @@ char *create_array(unsigned int size, char c)
 {
 	char *a;
 	unsigned int i;
+	if (size == 0)
+		return (NULL);
 
 	a = malloc(sizeof(c) * size);
 
-	if (size == 0 && a == 0)
+	if (a == 0)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
 		a[i] = c;
+
 	return (a);
 }
