287afa910b6452dd8b62179ef8b9d0d985e87ccd
fevies <elviesodhiambo@gmail.com>
2023-09-06 14:09:42 -0700

    prints name then adds new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *main - prints name followed by a new line
+ *main - entry point
  *
- *@argc: number of command line arguments.
+ *@argc: count
  *@argv: argument velocity
  *
  *Return: zer0
  */
-int main(int argc __attribute((unused)) , char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
