b24df47d144f76be6b0951b5e72ede9fe250f638
fevies <elviesodhiambo@gmail.com>
2023-09-06 15:10:16 -0700

    adds positive numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - proograms entry point
+ *@argc: count number of arguments
+ *@argv: argument vector
+ *Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+		printf("%d\n", resureturn 0;
+}
