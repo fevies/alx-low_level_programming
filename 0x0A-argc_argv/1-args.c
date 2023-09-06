ddd0de87a546c565d18bfa4c9ede9e44c45ef3a4
fevies <elviesodhiambo@gmail.com>
2023-09-06 14:28:54 -0700

    prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - programs entry point
+ *@argc: count command line
+ *@argv: argument velocity
+ *
+ *Return: zero
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
