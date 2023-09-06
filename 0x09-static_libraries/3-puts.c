32db23d5d752489592a472b392dc6ec73b858627
fevies <elviesodhiambo@gmail.com>
2023-09-06 08:12:50 -0700

    added c output

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -5,9 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-void _puts(char *str)
+void _puts(char *str);
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
