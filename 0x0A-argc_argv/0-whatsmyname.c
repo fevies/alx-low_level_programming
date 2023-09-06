451d68c6eef3d24971dc8715b76dfe39e996acea
fevies <elviesodhiambo@gmail.com>
2023-09-06 13:20:58 -0700

    prints name followed by new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,13 @@
 /**
  *main - prints name followed by a new line
  *
- *argc: number of command line arguments.
+ *@argc: number of command line arguments.
  *@argv: argument velocity
  *
  *Return: zer0
  */
-int main(int argc, char *argv[])
+int main(int argc__attribute__((unused)), char *argv[])
 {
-	(void)argc;
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
