22db1eb053a8d3c781babdcdab5fc725a91d671a
fevies <{ID}+{username}@users.noreply.github.com>
2023-10-11 03:23:50 -0700

    updated main header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char *s);
 void  _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -5,3 +7,4 @@ int factorial(int n);
 int _pow_recursion(int x,int y);
 int _sqrt_recursion(int n);
 int is_prine_number(int n);
+#endif
