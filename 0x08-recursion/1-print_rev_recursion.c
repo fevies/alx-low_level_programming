42c1de38c5d1a36974448af8f4fdfacd670828e8
fevies <{ID}+{username}@users.noreply.github.com>
2023-10-11 04:22:56 -0700

    prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,14 +4,11 @@
  *@s: pointer to string;
  *Return: Always zero;
  */
-
 void _print_rev_recursion(char *s)
 {
-        if (*s)
-        {
-                _print_rev_recursiion(s + 1);
-        }
-        else
-        {
-                _putchar(*s);
-        }
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
